a2bd17dd3f012e0d73cf9f819215d2939c769aee
さくら <sakurasatodev@gmail.com>
2021-12-27 17:31:34 +0000

    :rocket: add supported tokens

--- contracts/SakuraSwap.sol
+++ contracts/SakuraSwap.sol
@@ -1,6 +1,27 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SakuraSwap {
-    string private greeting;
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SakuraSwap is Ownable {
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    EnumerableSet.AddressSet private _supportedTokens;
+
+    constructor() {}
+
+    function supportedTokens() external view returns (address[] memory) {
+        uint256 length = _supportedTokens.length();
+        address[] memory tokens = new address[](length);
+        for (uint256 i = 0; i < length; i++) {
+            tokens[i] = _supportedTokens.at(i);
+        }
+        return tokens;
+    }
+
+    function addSupportedToken(address token) external onlyOwner {
+        require(!_supportedTokens.contains(token), "Token already supported");
+        _supportedTokens.add(token);
+    }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,31 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { SakuraSwap } from "../typechain";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+const SUSHI = "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2"
+const YFI = "0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e"
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+describe("Supported Tokens", () => {
+  let sakuraSwap: SakuraSwap;
+  it("Should deploy contract", async () => {
+    const SakuraSwap = await ethers.getContractFactory("SakuraSwap");
+    sakuraSwap = await SakuraSwap.deploy();
+    await sakuraSwap.deployed();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
+  it("Should be empty by default", async () => {
+    expect(await sakuraSwap.supportedTokens()).to.be.empty;
+  })
+  it("Should support adding", async () => {
+    expect(await sakuraSwap.supportedTokens()).to.be.empty;
+    await (await sakuraSwap.addSupportedToken(SUSHI)).wait();
+    expect(await sakuraSwap.supportedTokens()).to.eql([SUSHI]);
+    await (await sakuraSwap.addSupportedToken(YFI)).wait();
+    expect(await sakuraSwap.supportedTokens()).to.eql([SUSHI, YFI]);
+  })
+  it("Should revert already supported", async () => {
+    await expect(
+      sakuraSwap.addSupportedToken(YFI)
+    ).to.be.revertedWith("Token already supported");
+  })
 });
