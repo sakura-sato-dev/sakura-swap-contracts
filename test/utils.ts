459a61ae99d13f5f06d5a78f90b70db09f7e04ca
さくら <sakurasatodev@gmail.com>
2021-12-28 11:11:11 +0000

    :sparkles: add deposit utility

--- test/utils.ts
+++ test/utils.ts
@@ -1,6 +1,7 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
+import { SakuraSwap } from "../typechain";
 
 
 export const impersonateAddress = async (address: string) => {
@@ -12,7 +13,7 @@ export const impersonateAddress = async (address: string) => {
     return ethers.provider.getSigner(address);
 };
 
-export const scale = (value: number) => {
+export const scale = (value: number): BigNumber => {
     return BigNumber.from(value).mul(BigNumber.from(10).pow(18));
 }
 
@@ -22,8 +23,16 @@ export const approve = async (token: string, contract: string, signer: any) => {
     await (await sushi.approve(contract, scale(100_000_000))).wait();
 }
 
-export const balanceOf = async (owner: SignerWithAddress, token: string, account: string) => {
+export const balanceOf = async (owner: SignerWithAddress, token: string, account: string): Promise<BigNumber> => {
     const LpToken = await ethers.getContractFactory("LpToken");
     const sushi = new ethers.Contract(token, LpToken.interface, owner);
-    return sushi.balanceOf(account);
+    return BigNumber.from(await sushi.balanceOf(account));
 }
+
+export const deposit = async (sakuraSwap: SakuraSwap, token: string, account: string) => {
+    await (await sakuraSwap.addSupportedToken(token, "Sakura Sushi", "sakSUSHI")).wait();
+    const signer = await impersonateAddress(account);
+    await approve(token, sakuraSwap.address, signer);
+    await (await sakuraSwap.connect(signer).deposit(token, scale(100))).wait();
+}
+
