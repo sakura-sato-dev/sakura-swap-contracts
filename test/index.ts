98b602d2c39f7bcc4e440d159c945a84e7af8f48
さくら <sakurasatodev@gmail.com>
2021-12-28 11:48:56 +0000

    ✅ add tests for swapping

--- test/index.ts
+++ test/index.ts
@@ -3,12 +3,13 @@ import { expect } from "chai";
 import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 import { SakuraSwap } from "../typechain";
-import { balanceOf, deposit, scale } from "./utils";
+import { amountIn, amountOut, balanceOf, deposit, scale, swapIn, swapOut } from "./utils";
 
 const SUSHI = "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2"
 const YFI = "0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e"
 
 const SUSHI_OWNER = "0xd96Dd2337d964514Eb7E2E50d8Eea0d846feC960"
+const YFI_OWNER = "0x53c286E0AbE87c9e6d4d95ebE62ceaFa4aFCE849"
 
 describe("Sakura Swap Contract", () => {
   let sakuraSwap: SakuraSwap;
@@ -76,6 +77,51 @@ describe("Sakura Swap Contract", () => {
       expect(await balanceOf(owner, SUSHI, sakuraSwap.address)).to.equal(scale(100));
       const lpToken = await sakuraSwap.lpTokens(SUSHI);
       expect(await balanceOf(owner, lpToken, SUSHI_OWNER)).to.equal(scale(100));
+    })
+  })
+
+  describe("Swapping", () => {
+    it("Should revert for unsupported token in", async () => {
+      await deposit(sakuraSwap, YFI, YFI_OWNER);
+      await expect(
+        sakuraSwap.swapOut(SUSHI, YFI, scale(10))
+      ).to.be.revertedWith("Token not supported");
+      await expect(
+        sakuraSwap.swapIn(SUSHI, YFI, scale(10))
+      ).to.be.revertedWith("Token not supported");
+    })
+    it("Should revert for unsupported token out", async () => {
+      await deposit(sakuraSwap, SUSHI, SUSHI_OWNER);
+      await expect(
+        sakuraSwap.swapOut(SUSHI, YFI, scale(10))
+      ).to.be.revertedWith("Token not supported");
+      await expect(
+        sakuraSwap.swapIn(SUSHI, YFI, scale(10))
+      ).to.be.revertedWith("Token not supported");
+    })
+    it("Should swap in", async () => {
+      await deposit(sakuraSwap, SUSHI, SUSHI_OWNER);
+      await deposit(sakuraSwap, YFI, YFI_OWNER);
+      const sushiBalanceBefore = await balanceOf(owner, SUSHI, SUSHI_OWNER);
+      const yfiBalanceBefore = await balanceOf(owner, YFI, SUSHI_OWNER);
+      const expected = await amountOut(sakuraSwap, SUSHI, YFI);
+      await swapIn(sakuraSwap, SUSHI, YFI, SUSHI_OWNER);
+      const sushiBalanceAfter = await balanceOf(owner, SUSHI, SUSHI_OWNER);
+      const yfiBalanceAfter = await balanceOf(owner, YFI, SUSHI_OWNER);
+      expect(sushiBalanceBefore.sub(sushiBalanceAfter)).to.equal(scale(10));
+      expect(yfiBalanceAfter.sub(yfiBalanceBefore)).to.equal(expected);
+    })
+    it("Should swap out", async () => {
+      await deposit(sakuraSwap, SUSHI, SUSHI_OWNER);
+      await deposit(sakuraSwap, YFI, YFI_OWNER);
+      const sushiBalanceBefore = await balanceOf(owner, SUSHI, SUSHI_OWNER);
+      const yfiBalanceBefore = await balanceOf(owner, YFI, SUSHI_OWNER);
+      const needed = await amountIn(sakuraSwap, SUSHI, YFI);
+      await swapOut(sakuraSwap, SUSHI, YFI, SUSHI_OWNER, needed);
+      const sushiBalanceAfter = await balanceOf(owner, SUSHI, SUSHI_OWNER);
+      const yfiBalanceAfter = await balanceOf(owner, YFI, SUSHI_OWNER);
+      expect(sushiBalanceBefore.sub(sushiBalanceAfter)).to.equal(needed);
+      expect(yfiBalanceAfter.sub(yfiBalanceBefore)).to.equal(scale(10));
     })
   })
 })
