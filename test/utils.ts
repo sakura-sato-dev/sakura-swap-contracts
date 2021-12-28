c6d03112052e5dcea130250ef0f7a44668fbec63
さくら <sakurasatodev@gmail.com>
2021-12-28 13:02:25 +0000

    :sparkles: add utilities for getting exchnage rate

--- test/utils.ts
+++ test/utils.ts
@@ -2,6 +2,7 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 import { SakuraSwap } from "../typechain";
+import { tokenData } from "./data";
 
 
 export const impersonateAddress = async (address: string) => {
@@ -29,19 +30,24 @@ export const balanceOf = async (owner: SignerWithAddress, token: string, account
     return BigNumber.from(await sushi.balanceOf(account));
 }
 
+export const addSupportedToken = async (sakuraSwap: SakuraSwap, token: string) => {
+    const data = tokenData[token];
+    await (await sakuraSwap.addSupportedToken(token, "Sakura " + data.name, "sak" + data.symbol, data.oracle)).wait();
+}
+
 export const deposit = async (sakuraSwap: SakuraSwap, token: string, account: string) => {
-    await (await sakuraSwap.addSupportedToken(token, "Mock Name", "SYMBOL")).wait();
+    await addSupportedToken(sakuraSwap, token);
     const signer = await impersonateAddress(account);
     await approve(token, sakuraSwap.address, signer);
     await (await sakuraSwap.connect(signer).deposit(token, scale(100))).wait();
 }
 
-export const amountIn = async (sakuraSwap: SakuraSwap, tokenIn: string, tokenOut: string, amount: BigNumber = scale(10)) => {
-    return BigNumber.from(await sakuraSwap.amountIn(tokenIn, tokenOut, amount));
+export const getAmountIn = async (sakuraSwap: SakuraSwap, tokenIn: string, tokenOut: string, amount: BigNumber = scale(10)) => {
+    return BigNumber.from(await sakuraSwap.getAmountIn(tokenIn, tokenOut, amount));
 }
 
-export const amountOut = async (sakuraSwap: SakuraSwap, tokenIn: string, tokenOut: string, amount: BigNumber = scale(10)) => {
-    return BigNumber.from(await sakuraSwap.amountOut(tokenIn, tokenOut, amount));
+export const getAmountOut = async (sakuraSwap: SakuraSwap, tokenIn: string, tokenOut: string, amount: BigNumber = scale(10)) => {
+    return BigNumber.from(await sakuraSwap.getAmountOut(tokenIn, tokenOut, amount));
 }
 
 export const swapIn = async (sakuraSwap: SakuraSwap, tokenIn: string, tokenOut: string, account: string, amount: BigNumber = scale(10)) => {
@@ -54,4 +60,8 @@ export const swapOut = async (sakuraSwap: SakuraSwap, tokenIn: string, tokenOut:
     const signer = await impersonateAddress(account);
     await approve(tokenIn, sakuraSwap.address, signer);
     await (await sakuraSwap.connect(signer).swapOut(tokenIn, tokenOut, amount)).wait();
+}
+
+export const getExchangeRate = async (sakuraSwap: SakuraSwap, tokenIn: string, tokenOut: string) => {
+    return BigNumber.from(await sakuraSwap.getExchanageRate(tokenIn, tokenOut));
 }
